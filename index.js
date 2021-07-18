f0b6f151504bd729e684a49ef6e291782fc5d316
tushar9891 <82775995+tushar9891@users.noreply.github.com>
2021-07-18 16:18:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,31 +19,31 @@ function makesound(key){
 
 switch (key) {
   case "w":
-    var audio = new Audio("sounds/tom-1.mp3");
+    var audio = new Audio("tom-1.mp3");
     audio.play();
     break;
   case "a":
-    var audio = new Audio("sounds/tom-2.mp3");
+    var audio = new Audio("tom-2.mp3");
     audio.play();
     break;
   case "s":
-    var audio = new Audio("sounds/tom-3.mp3");
+    var audio = new Audio("tom-3.mp3");
     audio.play();
     break;
   case "d":
-    var audio = new Audio("sounds/tom-4.mp3");
+    var audio = new Audio("tom-4.mp3");
     audio.play();
     break;
   case "j":
-    var audio = new Audio("sounds/snare.mp3");
+    var audio = new Audio("snare.mp3");
     audio.play();
     break;
   case "k":
-    var audio = new Audio("sounds/crash.mp3");
+    var audio = new Audio("crash.mp3");
     audio.play();
     break;
   case "l":
-    var audio = new Audio("sounds/kick-bass.mp3");
+    var audio = new Audio("kick-bass.mp3");
     audio.play();
     break;
   default:
@@ -61,5 +61,3 @@ function buttonAnimation(pressed){
 }
 
 
-//var audio=new Audio("sounds/tom-1.mp3");
-//audio.play();
